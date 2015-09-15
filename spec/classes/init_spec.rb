580b335ca9bc5482a10aceca4c1ce76b68c4dee8
PierrickI3 <pierrick.lozach@inin.com>
2015-09-15 11:41:56 +0200

    Updating tests (removing version parameter)

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -28,20 +28,11 @@ describe 'dialer' do
     end
   end
 
-  context 'should fail if ensure is not set to installed is not set' do
-    let(:params) {{ :product => 'ODS', :ensure => 'clearly not installed', :ccsservername => 'testccs' }}
-    it do
-      expect {
-        should contain_class('dialer') 
-      }.to raise_error(Puppet::Error, /Must pass version to Class\[Dialer\]/)
-    end
-  end
-
   # ---------------
   # Parameter tests
   # ---------------
   context 'should fail if product parameter is empty' do
-  	let(:params) {{ :product => '', :ensure => 'installed', :version => '2015R2', :ccsservername => 'testccs' }}
+  	let(:params) {{ :product => '', :ensure => 'installed', :ccsservername => 'testccs' }}
   	it do
   	  expect {
   	    should contain_class('dialer') 
@@ -50,7 +41,7 @@ describe 'dialer' do
   end
 
   context 'should fail if ensure parameter is not set to installed' do
-  	let(:params) {{ :product => 'ODS', :ensure => '', :version => '2015R2', :ccsservername => 'testccs' }}
+  	let(:params) {{ :product => 'ODS', :ensure => '', :ccsservername => 'testccs' }}
   	it do
   	  expect {
   	    should contain_class('dialer') 
@@ -58,17 +49,8 @@ describe 'dialer' do
   	end
   end
 
-  context 'should fail if version parameter is not set' do
-  	let(:params) {{ :product => 'ODS', :ensure => 'installed', :ccsservername => 'testccs' }}
-  	it do
-  	  expect {
-  	    should contain_class('dialer') 
-  	  }.to raise_error(Puppet::Error, /Must pass version to Class\[Dialer\]/)
-  	end
-  end
-
   context 'should fail if ccsservername parameter is not set' do
-  	let(:params) {{ :product => 'ODS', :ensure => 'installed', :version => '2015_R2' }}
+  	let(:params) {{ :product => 'ODS', :ensure => 'installed' }}
   	it do
   	  expect {
   	    should contain_class('dialer') 
@@ -91,22 +73,22 @@ describe 'dialer' do
   end
 
   context 'should NOT have a CCS package when ODS is requested' do
-    let(:params) {{ :product => 'ODS', :ensure => 'installed', :version => '2015R2', :ccsservername => 'testccs' }}
+    let(:params) {{ :product => 'ODS', :ensure => 'installed', :ccsservername => 'testccs' }}
     it { should_not contain_package('dialer-ccs-install') }
   end
 
   context 'should NOT create a sql file to create the database' do
-    let(:params) {{ :product => 'ODS', :ensure => 'installed', :version => '2015R2', :ccsservername => 'testccs' }}
+    let(:params) {{ :product => 'ODS', :ensure => 'installed', :ccsservername => 'testccs' }}
     it {should_not contain_file('c:/tmp/createdatabase.sql') }
   end
 
   context 'should NOT create the database' do
-    let(:params) {{ :product => 'ODS', :ensure => 'installed', :version => '2015R2', :ccsservername => 'testccs' }}
+    let(:params) {{ :product => 'ODS', :ensure => 'installed', :ccsservername => 'testccs' }}
     it {should_not contain_exec('create-sql-database') }
   end
 
   context 'should NOT create a udl file' do
-    let(:params) {{ :product => 'ODS', :ensure => 'installed', :version => '2015R2', :ccsservername => 'testccs' }}
+    let(:params) {{ :product => 'ODS', :ensure => 'installed', :ccsservername => 'testccs' }}
     it {should_not contain_file('c:/users/vagrant/desktop/connection.udl') }
   end
 
@@ -114,37 +96,37 @@ describe 'dialer' do
   # CCS
   # ---
   context 'should not have an ODS package when CCS is requested' do
-    let(:params) {{ :product => 'CCS', :ensure => 'installed', :version => '2015R2', :ccsservername => '' }}
+    let(:params) {{ :product => 'CCS', :ensure => 'installed', :ccsservername => '' }}
     it { should_not contain_package('dialer-ods-install') }
   end
 
   context 'should have a CCS package' do
-  	let(:params) {{ :product => 'CCS', :ensure => 'installed', :version => '2015R2', :ccsservername => '' }}
+  	let(:params) {{ :product => 'CCS', :ensure => 'installed', :ccsservername => '' }}
   	it { should contain_package('dialer-ccs-install') }
   end
 
   context 'should install sql 2008 r2 native client' do
-    let(:params) {{ :product => 'CCS', :ensure => 'installed', :version => '2015R2', :ccsservername => '' }}
+    let(:params) {{ :product => 'CCS', :ensure => 'installed', :ccsservername => '' }}
     it { should contain_package('sql2008r2.nativeclient')}
   end
 
   context 'shoud install SQL server' do
-    let(:params) {{ :product => 'CCS', :ensure => 'installed', :version => '2015R2', :ccsservername => '' }}
+    let(:params) {{ :product => 'CCS', :ensure => 'installed', :ccsservername => '' }}
     it { should contain_class('sqlserver')}
   end
 
   context 'should create a sql file to create the database' do
-    let(:params) {{ :product => 'CCS', :ensure => 'installed', :version => '2015R2', :ccsservername => '' }}
+    let(:params) {{ :product => 'CCS', :ensure => 'installed', :ccsservername => '' }}
     it {should contain_file('c:/tmp/createdatabase.sql') }
   end
 
   context 'should create the database' do
-    let(:params) {{ :product => 'CCS', :ensure => 'installed', :version => '2015R2', :ccsservername => '' }}
+    let(:params) {{ :product => 'CCS', :ensure => 'installed', :ccsservername => '' }}
     it {should contain_exec('create-sql-database') }
   end
 
   context 'should create a udl file' do
-    let(:params) {{ :product => 'CCS', :ensure => 'installed', :version => '2015R2', :ccsservername => '' }}
+    let(:params) {{ :product => 'CCS', :ensure => 'installed', :ccsservername => '' }}
     it {should contain_file('C:/I3/IC/Server/UDL/connection.udl') }
   end
 
